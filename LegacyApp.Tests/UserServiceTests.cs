4db861bf8ba2a1b57dafdc65e6d0d88bbe61d6d8
Kacpers27325 <s27325@pjwstk.edu.pl>
2024-03-30 22:02:34 +0100

    Pisanie testow

--- LegacyApp.Tests/UserServiceTests.cs
+++ LegacyApp.Tests/UserServiceTests.cs
@@ -43,6 +43,48 @@ public class UserServiceTests
         Assert.Throws<ArgumentException>(result);
     }
     
+    // AddUser_ReturnsFalseWhenMissingAtSignAndDotInEmail
+    [Fact]
+    public void AddUser_ReturnsFalseWhenMissingAtSignAndDotInEmail()
+    {
+        //Arrange
+        var userService = new UserService();
+        
+        //Act
+        var result = userService.AddUser(null, "K", "aaaacom",DateTime.Parse("1992-02-03"),10);
+
+        //Assert
+        Assert.False(result);
+    }
+    
+    
+    // AddUser_ReturnsFalseWhenYoungerThen21YearsOld
+    [Fact]
+    public void AddUser_ReturnsFalseWhenYoungerThen21YearsOld()
+    {
+        //Arrange
+        var userService = new UserService();
+        
+        //Act
+        var result =  userService.AddUser("Mietek", "K", "aaa@aaa.com",DateTime.Parse("2022-02-03"),10);
+        
+
+        //Assert
+        Assert.False(result);
+    }
+    
+    
+    // AddUser_ReturnsTrueWhenVeryImportantClient
+    // AddUser_ReturnsTrueWhenImportantClient
+    // AddUser_ReturnsTrueWhenNormalClient
+    // AddUser_ReturnsFalseWhenNormalClientWithNoCreditLimit
+    // AddUser_ThrowsExceptionWhenUserDoesNotExist
+    // AddUser_ThrowsExceptionWhenUserNoCreditLimitExistsForUser
+    
+    
+    
+    
+    
     
     
 }
